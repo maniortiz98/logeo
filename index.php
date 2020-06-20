cc44cab5cd00b63ae6069664ef1acb705438271d
mani <maniortiz98@gmail.com>
2020-06-19 19:16:33 -0500

    logeo

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+	
+$alert = '';
+session_start();
+if(!empty($_SESSION['active']))
+{
+	header('location: sistema/');
+}else{
+
+	if(!empty($_POST))
+	{
+		if(empty($_POST['usuario']) || empty($_POST['clave']))
+		{
+			$alert = 'Ingrese su usuario y su calve';
+		}else{
+
+			require_once "conexion.php";
+
+			$user = mysqli_real_escape_string($conection,$_POST['usuario']);
+			$pass = md5(mysqli_real_escape_string($conection,$_POST['clave']));
+
+			$query = mysqli_query($conection,"SELECT * FROM usuario WHERE usuario= '$user' AND clave = '$pass'");
+			mysqli_close($conection);
+			$result = mysqli_num_rows($query);
+
+			if($result > 0)
+			{
+				$data = mysqli_fetch_array($query);
+				$_SESSION['active'] = true;
+				$_SESSION['idUser'] = $data['idusuario'];
+				$_SESSION['nombre'] = $data['nombre'];
+				$_SESSION['email']  = $data['email'];
+				$_SESSION['user']   = $data['usuario'];
+				$_SESSION['rol']    = $data['rol'];
+
+				header('location: sistema/');
+			}else{
+				$alert = 'El usuario o la clave son incorrectos';
+				session_destroy();
+			}
+
+
+		}
+
+	}
+}
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>logeo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div>
+	<h3>Usuario: admin</h3>
+	<h3>contraseña: 123</h3>
+
+	</div>
+	
+	<section id="container">
+		
+		
+		<form action="" method="post">
+			
+			<h3>Iniciar Sesión</h3>
+			<img src="img/1.png" alt="Login" width="200px">
+			
+
+			<input type="text" name="usuario" placeholder="Usuario">
+			<input type="password" name="clave" placeholder="Contraseña">
+			<div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
+			<input type="submit" value="INGRESAR">
+
+		</form>
+
+	</section>
+</body>
+</html>
---
+++ sistema/index.php
@@ -0,0 +1,23 @@
+<?php 
+	session_start();
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+	<title>Prueva</title>
+</head>
+<body>
+	
+	<section id="container">
+		<h1>Bienvenido a una prueva</h1>
+
+		<a href="salir.php">salir</a>
+
+		<p><a href="http://localhost/codigo/php/">Volver al pagina PHP</a></p>
+	</section>
+	
+</body>
+</html>
